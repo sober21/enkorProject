83c05937fc9d430c50de161e371d3b176b237018
trezvypapa <mix_mmg@mail.ru>
2024-09-23 23:47:57 +0300

    Роутер подключен к главному приложению в main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 import uvicorn
 
 from fastapi import FastAPI
+from api import router
+
 
 app  = FastAPI()
+app.include_router(router, prefix="/api")
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
