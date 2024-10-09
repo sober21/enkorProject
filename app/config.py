4f6a6b15126972362810cf1c259f82ebc9d6ade2
trezvypapa <mix_mmg@mail.ru>
2024-10-09 22:51:04 +0300

    Добавил в класс Settings naming_convention

--- app/config.py
+++ app/config.py
@@ -1,13 +1,19 @@
 import os
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_path: str = os.getenv("DB_PATH")
-
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 setting = Settings()
