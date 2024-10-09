007089a23741b9ee2e27602fbb6d0a06882aa2c8
trezvypapa <mix_mmg@mail.ru>
2024-10-09 22:51:56 +0300

    Добавил в базовый класс Base naming_convention

--- app/database/models/base.py
+++ app/database/models/base.py
@@ -1,4 +1,10 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+from config import setting
+
+
 class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=setting.naming_convention)
+
     id: Mapped[int] = mapped_column(primary_key=True)
