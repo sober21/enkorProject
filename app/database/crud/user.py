6827916880c7b55a450b5a31d0eeec32354dd9ef
trezvypapa <mix_mmg@mail.ru>
2024-10-24 22:28:48 +0300

    Удалил неиспользуемые импорты

--- app/database/crud/user.py
+++ app/database/crud/user.py
@@ -1,9 +1,8 @@
 from sqlalchemy import select
-from sqlalchemy.orm import Session, scoped_session
+from sqlalchemy.orm import Session
 
 from database import engine
-from database.models import User, Salary, Employee, Profile, Order
-from database.models.base import Base
+from database.models import User
 
 
 def create_user(session: Session, login: str, hash_password: str) -> User:
